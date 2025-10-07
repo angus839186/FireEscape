504425cd3f3cd6334a0befc29d890314c76ae4d8
徐英森 <96182289+angus839186@users.noreply.github.com>
2025-10-07 20:44:23 +0800

    角色生成功能_場景測試用

--- Assets/angus/Scripts/Player/PlayerSpawner.cs
+++ Assets/angus/Scripts/Player/PlayerSpawner.cs
@@ -7,10 +7,26 @@ public class PlayerSpawner : MonoBehaviour
     public Vector3 SpawnPosition => transform.position;
     public Vector3 SpawnEuler => transform.eulerAngles;
 
+    public GameObject PlayerPrefab;
+
+    public GameObject GameInputPrefab;
+
+    void Awake()
+    {
+        if (FindFirstObjectByType<GameManager>() != null) return;
+        SpawnPlayer();
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, 0.3f);
         var forward = Quaternion.Euler(transform.eulerAngles) * Vector3.forward;
         Gizmos.DrawLine(transform.position, transform.position + forward * 1.0f);
+    }
+    public void SpawnPlayer()
+    {
+        Instantiate(GameInputPrefab);
+        Quaternion rot = Quaternion.Euler(SpawnEuler);
+        Instantiate(PlayerPrefab, SpawnPosition, rot);
     }
 }
